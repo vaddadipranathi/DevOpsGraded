c4b45c4aed4849f83f30f6a50f0ae136802cf41f
vaddadipranathi <vaddadipranathi0@gmail.com>
2021-06-06 14:54:40 +0530

    Intial Commit

---
+++ src/main/java/com/hcl/DevOpsGraded/DevOpsGradedApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.DevOpsGraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/hcl/DevOpsGraded/DevOpsGradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.DevOpsGraded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsGradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
